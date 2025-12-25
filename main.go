b259d2559d524248e49ddaf8cacc4fca352c62b1
serkenn <kenchiku3606@gmail.com>
2025-12-25 15:01:23 +0900

    Fix: Explicit import alias and remove test job

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
-import "github.com/serkenn/zabbix-smartmeter/lib"
+// importの前に "mpsm" を付けて、この名前で使うことを宣言します
+import mpsm "github.com/serkenn/zabbix-smartmeter/lib"
 
 func main() {
 	mpsm.Do()
